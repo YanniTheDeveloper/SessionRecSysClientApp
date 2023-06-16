51b2358bb703d7bebde2832b81dd6746e496cb1e
Naol Ketema <naolketema55@gmail.com>
2023-06-16 16:39:02 +0300

    api class added

---
+++ lib/data/api.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiProvider {
+//   //List<dynamic> data = [];
+
+Future<dynamic> fetchData() async {
+  var url = Uri.parse('http://backendsession.onrender.com/home');
+  var response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    var jsonData = jsonDecode(response.body);
+    // print(jsonData);
+
+    return jsonData;
+  } else {
+    throw Exception('Failed to load data');
+  }
+}
+}
+
+
